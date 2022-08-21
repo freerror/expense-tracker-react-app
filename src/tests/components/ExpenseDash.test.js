14b195319e4df64cce5e33eb4a829cb782e7c42a
appuser <appuser@fastmail.com>
2022-08-21 12:51:24 +1200

    testing: add mock and update snapshots

--- src/tests/components/ExpenseDash.test.js
+++ src/tests/components/ExpenseDash.test.js
@@ -6,6 +6,8 @@ import '@testing-library/jest-dom'
 import ExpenseDash from "../../components/ExpenseDash"
 import { renderWithWrappers } from '../../utils/test-utils'
 
+jest.mock('react-fit')
+
 test("Test expense list item", () => {
   const { asFragment } = renderWithWrappers(<ExpenseDash />)
   expect(asFragment()).toMatchSnapshot()
