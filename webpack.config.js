e3e2159b85dfe627e399e97e81bb08af33732bba
appuser <appuser@fastmail.com>
2022-08-28 00:05:33 +1200

    Continue updating for production
    Use webpack arguments to determine whether environment is prod or dev.
    Provide CSS extraction in dev and prod scenarios.
    
    Also switched over to generated HTML file with the HtmlWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,82 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// entry -> output
-module.exports = {
-  entry: './src/app.jsx',
-  // entry: './src/playground/redux-expensify.js',
-  // entry: './src/playground/hoc.jsx',
-  // entry: './src/playground/redux-101.js',
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.join(__dirname, 'public/scripts')
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [{
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      },
+module.exports = (_, { mode }) => {
+  prod = mode === "production"
+  return {
+    plugins: [
+      // plugins to run regardless
+      new HtmlWebpackPlugin({
+        title: 'Development',
+        templateContent: `
+        <html>
+            <body>
+              <div id="app"></div>
+            </body>
+        </html>`
+      }),
+    ].concat(prod ? [
+      // plugins to run in production
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      })
+    ] : [
+      // plugins to run in development
+    ]),
+    target: 'web',
+    entry: './src/app.jsx',
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
     },
-    {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    port: 9000,
-    historyApiFallback: true
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [{
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          prod ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+        ],
+      }]
+    },
+    mode: 'development',
+    devtool: 'eval',
+    devServer: {
+      static: './dist',
+    },
+    optimization: {
+
+      runtimeChunk: 'single',
+
+    },
   }
 };
