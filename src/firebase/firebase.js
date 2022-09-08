c761ec5f81a3ee8d0fd078ff99da9cb1fe0cd14d
appuser <appuser@fastmail.com>
2022-09-09 00:24:28 +1200

    add environmental variables for firebase config
    This provides dev and prod environments pointing to different firestores.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDV04Oq50Y4uB3h9vZZw6nGxdk_7d3BA-M",
-  authDomain: "xp3ns-1337.firebaseapp.com",
-  projectId: "xp3ns-1337",
-  storageBucket: "xp3ns-1337.appspot.com",
-  messagingSenderId: "934659011074",
-  appId: "1:934659011074:web:b9137404e945676df96d1b",
-  measurementId: "G-3JEVVEPTGY"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (_, { mode }) => {
   prod = mode === "production"
+  console.log("Prod: ", prod);
+  if (prod) {
+    require('dotenv').config({ path: '.env.prod' })
+  } else {
+    require('dotenv').config({ path: '.env.dev' })
+  }
+
   return {
     experiments: {
       topLevelAwait: true
@@ -15,13 +23,22 @@ module.exports = (_, { mode }) => {
         filename: '../index.html',
         favicon: './public/dist/favicon.ico',
       }),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+      })
     ].concat(prod ? [
       // plugins to run in production
       new MiniCssExtractPlugin({
         filename: "[name].css",
         chunkFilename: "[id].css",
         ignoreOrder: false, // Enable to remove warnings about conflicting order
-      })
+      }),
     ] : [
       // plugins to run in development
     ]),
