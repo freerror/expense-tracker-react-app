f8ecbad7bc8800a10694633736185e543df54516
appuser <appuser@fastmail.com>
2022-09-14 23:02:23 +1200

    User auth logic for main outlet for the app
    (Header component seems appropriate as it contains the main react-router Outlet)
    
    Added log out button.
    Navigates back to the login page if user not authed.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,40 @@
 import React from "react"
-import { NavLink, Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { NavLink, Outlet, useNavigate } from "react-router-dom"
+import { startLogout } from "../slices/auth"
 import Heading from "./Heading"
 
-const Header = (props) => (
-  <>
-    <header>
-      <Heading />
-      <HeaderLinks pageList={{
-        Dashboard: "dashboard",
-        Create: "create",
-        Help: "help"
-      }} />
-    </header>
-    <Outlet />
-  </>
-)
+const Header = (props) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const auth = useSelector(state => state.auth)
+
+  const onLogout = () => {
+    dispatch(startLogout()).then(() => { navigate("/") })
+  }
+
+  useEffect(() => {
+    if (!auth.userCreds) {
+      navigate("/")
+    }
+  })
+
+  return (
+    <>
+      <header>
+        <Heading />
+        <button onClick={onLogout}>Logout</button>
+        <HeaderLinks {...{ onLogout }} pageList={{
+          Dashboard: "dashboard",
+          Create: "create",
+          Help: "help",
+        }} />
+      </header>
+      <Outlet />
+    </>
+  )
+}
 
 const HeaderLinks = (props) => (
   <div>
@@ -24,6 +44,7 @@ const HeaderLinks = (props) => (
           <NavLink to={pageRef}>{pageName}</NavLink>
         </li>
       ))}
+      <br></br>
     </ul>
   </div>
 )
